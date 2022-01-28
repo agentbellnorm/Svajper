2ae2ad1ae19b8cb4d7e7d1ca997af09e74117000
Morgan Bentell <morgan.bentell@gmail.com>
2022-01-29 00:50:01 +0100

    animate imperatively, dont use state. better performance

--- src/svajper/Svajper.tsx
+++ src/svajper/Svajper.tsx
@@ -1,7 +1,6 @@
 import React, {
   PointerEventHandler,
   PropsWithChildren,
-  useCallback,
   useRef,
   useState,
 } from "react";
@@ -48,6 +47,9 @@ const getKey = (child: any): React.Key => {
   return child.key as React.Key;
 };
 
+const translateXorY = (direction: Direction, px: number) =>
+  `translate${direction === "horizontal" ? "X" : "Y"}(${px}px)`;
+
 const Svajper = ({
   children,
   direction,
@@ -55,60 +57,61 @@ const Svajper = ({
   onDraggedToIndex,
   slideSize,
 }: Props) => {
-  const [dragDelta, setDragDelta] = useState<number>(0);
   const [dragging, setIsDragging] = useState<boolean>(false);
+  const dragDelta = useRef<number>(0);
   const touchStart = useRef<number>(0);
   const indexWhenDragStart = useRef<number>(0);
-  const parentRef = useRef(null);
+  const swiperRef = useRef<HTMLDivElement>(null);
+  const touchStartTranslate = useRef<string>();
 
   const numItems = React.Children.count(children);
 
   const isHorizontal = direction === "horizontal";
 
-  const onPointerDown: PointerEventHandler<HTMLDivElement> = useCallback(
-    (e) => {
-      setIsDragging(true);
-      touchStart.current = isHorizontal ? e.clientX : e.clientY;
-      indexWhenDragStart.current = selectedItemIndex;
-    },
-    [selectedItemIndex, isHorizontal]
-  );
+  const onPointerDown: PointerEventHandler<HTMLDivElement> = (e) => {
+    indexWhenDragStart.current = selectedItemIndex;
+    dragDelta.current = 0;
+    touchStart.current = isHorizontal ? e.clientX : e.clientY;
 
-  const onPointerMove: PointerEventHandler<HTMLDivElement> = useCallback(
-    (e) => {
-      if (dragging) {
-        setDragDelta(
-          (isHorizontal ? e.clientX : e.clientY) - touchStart.current
+    if (swiperRef.current) {
+      touchStartTranslate.current = swiperRef.current.style.transform;
+    }
+    setIsDragging(true);
+  };
+
+  const onPointerMove: PointerEventHandler<HTMLDivElement> = (e) => {
+    if (dragging) {
+      dragDelta.current =
+        (isHorizontal ? e.clientX : e.clientY) - touchStart.current;
+
+      if (swiperRef.current) {
+        swiperRef.current.style.transform = translateXorY(
+          direction,
+          -indexWhenDragStart.current * slideSize + dragDelta.current
         );
-        const threshold = slideSize / 2;
-        const shouldChangeSlide = Math.abs(dragDelta) > threshold;
-        if (shouldChangeSlide) {
-          let newIndex = clamp(
-            indexWhenDragStart.current + Math.round(-dragDelta / slideSize),
-            0,
-            React.Children.count(children) - 1
-          );
-          onDraggedToIndex(newIndex);
-        }
       }
-    },
-    [
-      touchStart,
-      dragging,
-      dragDelta,
-      indexWhenDragStart,
-      children,
-      onDraggedToIndex,
-      slideSize,
-      isHorizontal,
-    ]
-  );
 
-  const onPointerUp: PointerEventHandler<HTMLDivElement> = useCallback(() => {
+      let currentHighlightedIndex = clamp(
+        indexWhenDragStart.current + Math.round(-dragDelta.current / slideSize),
+        0,
+        numItems - 1
+      );
+
+      if (currentHighlightedIndex !== selectedItemIndex) {
+        onDraggedToIndex(currentHighlightedIndex);
+      }
+    }
+  };
+
+  const onPointerUp: PointerEventHandler<HTMLDivElement> = () => {
+    if (swiperRef.current) {
+      swiperRef.current.style.transform = translateXorY(
+        direction,
+        -selectedItemIndex * slideSize
+      );
+    }
     setIsDragging(false);
-    setDragDelta(0);
-    touchStart.current = 0;
-  }, []);
+  };
 
   const startIndexToRender = clamp(selectedItemIndex - 2, 0, numItems);
   const endIndexToRender = clamp(selectedItemIndex + 5, 0, numItems);
@@ -123,14 +126,12 @@ const Svajper = ({
         [styles.horizontal]: isHorizontal,
         [styles.vertical]: !isHorizontal,
       })}
-      ref={parentRef}
+      ref={swiperRef}
       style={{
-        transform: `translate${isHorizontal ? "X" : "Y"}(${
-          dragging
-            ? -indexWhenDragStart.current * slideSize + dragDelta
-            : -selectedItemIndex * slideSize
-        }px)`,
         transition: dragging ? "none" : undefined,
+        transform: dragging
+          ? touchStartTranslate.current
+          : translateXorY(direction, -selectedItemIndex * slideSize),
         width: isHorizontal ? `${numItems * slideSize}px` : undefined,
         height: !isHorizontal ? `${numItems * slideSize}px` : undefined,
       }}
